8c091789ddef07d6ae2f2cf89fb99f6cdb62ffa7
khantmawhein <kokhant228abc@gmail.com>
2021-08-13 08:30:48 +0630

    complete video 66

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,20 +5,21 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        // $request->validate(
-        //     [
-        //         'name' => ['required', 'string', 'max:255'],
-        //         'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-        //         'phone' => ['required', 'string', 'unique:users'],
-        //         'password' => ['required', 'string', 'min:8'],
-        //     ]
-        // );
+        $request->validate(
+            [
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'phone' => ['required', 'string', 'unique:users'],
+                'password' => ['required', 'string', 'min:8'],
+            ]
+        );
 
         $user = new User();
         $user->name = $request->name;
@@ -35,5 +36,23 @@ class AuthController extends Controller
 
         return success('Successfully Registered.', ['token' => $token]);
 
+    }
+
+    public function login(Request $request){
+        $request->validate(
+            [
+                'phone' => ['required', 'string'],
+                'password' => ['required', 'string'],
+            ]
+        );
+
+        if(Auth::attempt(['phone' => $request->phone, 'password' => $request->password])){
+            $user = auth()->user();
+            $token = $user->createToken('Magic Pay')->accessToken;
+
+            return success('Successfuly logined.', ['token' => $token]);
+        }
+
+        return fail('These credentials do not match our records.', '');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -25,5 +25,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::namespace('Api')->group(function(){
     Route::post('register', 'AuthController@register');
+    Route::post('login', 'AuthController@login');
 });
 
